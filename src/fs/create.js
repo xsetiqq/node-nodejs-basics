b96dea38164cbbe2b591db545c4128a35f27d112
Tsimafei <xsetiqq@gmail.com>
2025-04-27 20:42:30 +0200

    feat: add fs

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,24 @@
+import { writeFile, access } from "fs/promises";
+import { constants } from "fs";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+
+const pathToThisFile = fileURLToPath(import.meta.url);
+const directoryOfThisFile = dirname(pathToThisFile);
+
 const create = async () => {
-    // Write your code here 
+  const filePath = join(directoryOfThisFile, "files", "fresh.txt");
+
+  try {
+    await access(filePath, constants.F_OK);
+    throw new Error("FS operation failed");
+  } catch (err) {
+    if (err.code === "ENOENT") {
+      await writeFile(filePath, "I am fresh and young", "utf-8");
+    } else {
+      throw new Error("FS operation failed");
+    }
+  }
 };
 
 await create();
--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,31 @@
+import fs from "fs/promises";
+import { constants } from "fs";
+import { join, dirname } from "path";
+import { fileURLToPath } from "url";
+
+const pathToThisFile = fileURLToPath(import.meta.url);
+const directoryOfThisFile = dirname(pathToThisFile);
+
 const rename = async () => {
-    // Write your code here 
+  const filePath = join(directoryOfThisFile, "files", "wrongFilename.txt");
+  const filePathMd = join(directoryOfThisFile, "files", "wrongFilename.md");
+
+  try {
+    await fs.access(filePath, constants.F_OK);
+  } catch (err) {
+    throw new Error("FS operation failed");
+  }
+
+  try {
+    await fs.access(filePathMd, constants.F_OK);
+    throw new Error("FS operation failed");
+  } catch (err) {
+    if (err.code !== "ENOENT") {
+      throw new Error("FS operation failed");
+    }
+  }
+
+  await fs.rename(filePath, filePathMd);
 };
 
 await rename();
